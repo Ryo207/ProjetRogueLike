b899b8dbda12a34cddac6847f2ecc7597f9f672c
Ryo207 <b.decarvalho@rubika-edu.com>
2020-09-13 15:54:36 +0200

    [Ne Pas Merge] Commit de Backup : Je test des plugins de workflow

---
+++ Assets/BDC_Folder/BDC_Scripts/Bullet_Destroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Destroy : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
+
+
---
+++ Assets/BDC_Folder/BDC_Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variable Mouvement Perso
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    //Variable Tir 
+    public Camera cam;
+    Vector2 mousePos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+      movement.x =  Input.GetAxisRaw("Horizontal");
+      movement.y = Input.GetAxisRaw("Vertical");
+
+      
+        
+     mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDirection = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
