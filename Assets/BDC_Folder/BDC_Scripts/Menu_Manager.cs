a2f844e79cd4c0160c4b427b1d27ab195a58ca98
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2020-10-01 19:38:23 +0200

    [Ne pas merge] Implémentation menus

--- Assets/BDC_Folder/BDC_Scripts/Menu_Manager.cs
+++ Assets/BDC_Folder/BDC_Scripts/Menu_Manager.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class Menu_Manager : MonoBehaviour
 {
@@ -9,10 +12,35 @@ public class Menu_Manager : MonoBehaviour
     PlayerController playerController;
     public GameObject MenuPause;
     public GameObject MenuOptions;
+    public GameObject Credits;
+    public AudioMixer MainVolume;
+    Resolution[] resolution;
+    public Dropdown resolutionDropDown;
 
     private void Start()
     {
         playerController = GameObject.Find("Perso").GetComponent<PlayerController>();
+
+        resolution = Screen.resolutions;
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentRosolutionIndex = 0;
+        for (int i = 0; i < resolution.Length; i++)
+        {
+            string option = resolution[i].width + "x" + resolution[i].height;
+            options.Add(option);
+
+            if (resolution[i].width == Screen.currentResolution.width && resolution[i].height == Screen.currentResolution.height)
+            {
+                currentRosolutionIndex = i;
+
+            }
+        }
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentRosolutionIndex;
+        resolutionDropDown.RefreshShownValue();
 
     }
 
@@ -20,11 +48,6 @@ public class Menu_Manager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
 
-    }
-
-    public void playCredits()
-    {
-        SceneManager.LoadScene(2);
     }
 
     public void quitGame()
@@ -38,31 +61,59 @@ public class Menu_Manager : MonoBehaviour
         SceneManager.LoadScene(0);
 
     }
-
-    public void playOptions()
-    {
-        SceneManager.LoadScene(3);
-
-    }
     public void Resume()
     {
         MenuPause.SetActive(false);
+        MenuOptions.SetActive(false);
         playerController.stopTimePause = false;
         Time.timeScale = 1;
     }
 
-    public void OptionsInGame()
+    public void PlayOptions()
     {
-        StartCoroutine("OptionsInGame");
+        MenuPause.SetActive(false);
+        MenuOptions.SetActive(true);
+    }
+
+    public void QuitOptions()
+    {
+        MenuOptions.SetActive(false);
+        MenuPause.SetActive(true);
+    }
+
+    public void OptionsMainVolume(float volume)
+    {
+        MainVolume.SetFloat("MainVolume",volume);
 
     }
-    IEnumerator optionsingame()
-        {
+
+    public void PlayCredits()
+    {
         MenuPause.SetActive(false);
-        yield return new WaitForSeconds(0.1f);
+        MenuOptions.SetActive(false);
+        Credits.SetActive(true);
+    }
+
+    public void QuitCreditsToOptions()
+    {
+        Credits.SetActive(false);
         MenuOptions.SetActive(true);
-        playerController.stopTimePause = false;
-        }
+    }
+
+    public void QuitCreditToMenu()
+    {
+        Credits.SetActive(false);
+        MenuPause.SetActive(true);
+    }
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolutions = resolution[resolutionIndex];
+        Screen.SetResolution(resolutions.width, resolutions.height, Screen.fullScreen);
+    }
 
 
 
