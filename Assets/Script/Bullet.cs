da031380088bb9828ce145fb5cf2f35df6474cbf
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2020-10-04 17:27:02 +0200

    [Ne pas merge] Avancé système de tir

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 
@@ -7,9 +8,13 @@ public class Bullet : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public GameObject crosshair;
+    public GameObject perso;
 
     public float bulletForce = 20f;
 
+    Vector2 mousePos;
+    Vector2 persoPos;
 
     PlayerController playerController;
 
@@ -19,6 +24,10 @@ public class Bullet : MonoBehaviour
     }
     void Update()
     {
+        persoPos = new Vector2(perso.transform.position.x, perso.transform.position.y);
+        mousePos = playerController.cam.ScreenToWorldPoint(Input.mousePosition);
+        crosshair.transform.position = mousePos;
+
         if (Input.GetButtonDown("Fire1") && !playerController.stopTimePause) 
         {
             Shoot();
@@ -29,7 +38,7 @@ public class Bullet : MonoBehaviour
     {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        rb.AddForce(mousePos * bulletForce, ForceMode2D.Impulse);
 
     }
 }
