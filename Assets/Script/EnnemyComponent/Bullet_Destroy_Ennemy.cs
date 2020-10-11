87afc12cdebfe8218684037386f7a09e7e8c4bad
Ryo207 <b.decarvalho@rubika-edu.com>
2020-10-12 01:29:46 +0200

    [Ne Pas Merge] Pour Yovyov le bg ( si demain on a pas une bonne note je me jette du pont Villars)

---
+++ Assets/Script/EnnemyComponent/Bullet_Destroy_Ennemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Destroy_Ennemy : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Wall") || (col.gameObject.CompareTag("Player")))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
