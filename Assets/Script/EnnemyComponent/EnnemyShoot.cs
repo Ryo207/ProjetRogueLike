e5901439e75c768b70bcf41551375f411e7f9632
Ryo207 <b.decarvalho@rubika-edu.com>
2020-10-11 20:32:41 +0200

    Pour Yov le bg

--- Assets/Script/EnnemyComponent/EnnemyShoot.cs
+++ Assets/Script/EnnemyComponent/EnnemyShoot.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class EnnemyShoot : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    PathFinding pathFinding;
+
+    public float bulletForce;
+
+    public GameObject bulletPrefab;
+    public GameObject player;
+    public Transform firePoint;
 
     void Update()
     {
-        
+        if (pathFinding.FightingPhase == true)
+        {
+            Shoot();
+        }
+    
+    }
+    private void Shoot()
+    {
+        GameObject fireBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        fireBullet.GetComponent<Rigidbody2D>().velocity = firePoint.right * bulletForce;
+
     }
 }
