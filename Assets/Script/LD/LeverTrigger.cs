e15027a03a2534e9b953a00f736769287ad97c75
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2020-10-12 16:17:21 +0200

    Milestone 1 atteinte, début clean animator

--- Assets/Script/LD/LeverTrigger.cs
+++ Assets/Script/LD/LeverTrigger.cs
@@ -8,17 +8,24 @@ public class LeverTrigger : MonoBehaviour
     public UnityEngine.Experimental.Rendering.Universal.Light2D GlobalLight;
 
     public bool lightMilestone = false;
+    public Animator Light;
 
-
+    public void Start()
+    {
+        Light.SetBool("IsRed", false);
+        Light.SetBool("IsBlue",false);
+    }
     public void RedLight()
     {
         lightMilestone = false;
-        GlobalLight.color = Color.red;
+        Light.SetBool("IsRed", true);
+        Light.SetBool("IsBlue", false);
     }
 
     public void BlueLight()
     {
         lightMilestone = true;
-        GlobalLight.color = Color.blue;
+        Light.SetBool("IsBlue", true);
+        Light.SetBool("IsRed", false);
     }
 }
