5ed99f27594fc3f5d9c2540aa5e106a69be945e1
Ryo207 <b.decarvalho@rubika-edu.com>
2020-11-02 15:56:44 +0100

    [Commit De Backup]

---
+++ Assets/Script/LD/TilemapDestructor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilemapDestructor : MonoBehaviour
+{
+    [SerializeField]
+    PathFinding pathFinding;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (pathFinding.FightingPhase == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Script/Loot/ItemsSO.cs
+++ Assets/Script/Loot/ItemsSO.cs
@@ -1,13 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Items")]
 public class ItemsSO : ScriptableObject
 {
-    public string Itemname;
+    [HorizontalGroup("ScriptableObjectBaseHorizontal", 335)]
+    [PreviewField(100)]
+    [HideLabel]
     public Sprite Artwork;
+
+    [VerticalGroup("ScriptableObjectBaseVertical")]
+    [LabelWidth(100)]
+    public string Itemname;
+
+    [VerticalGroup("ScriptableObjectBaseVertical")]
+    [LabelWidth(100)]
     public int ID;
 
+    [TextArea]
+    [HideLabel]
+    [PropertySpace(SpaceBefore = 0, SpaceAfter = 40), PropertyOrder(1)]
+    [Title("Description", titleAlignment: TitleAlignments.Centered)]
+    public string description;
+
+
+
+    [EnumToggleButtons]
+    public ItemTypeEnum itemType;
+
+    [ShowIf("itemType", ItemTypeEnum.CacWeapon)]
+    public float damageCac;
+
+
+    [ShowIf("itemType", ItemTypeEnum.CacWeapon)]
+    public float reachCac;
+
+
+    [ShowIf("itemType", ItemTypeEnum.CacWeapon)]
+    public float rangeCac;
+
+    [ShowIf("itemType", ItemTypeEnum.CacWeapon)]
+    public float cooldownCac;
+
+    [EnumPaging]
+    [ShowIf("itemType", ItemTypeEnum.CacWeapon)]
+    public impactEffectCacEnum impactEffectCac;
+
+    [ShowIf("itemType", ItemTypeEnum.CacWeapon)]
+    public float weaponCooldownCac;
+
+    [ShowIf("itemType", ItemTypeEnum.DistanceWeapon)]
+    public float damageDistance;
+
+    [ShowIf("itemType", ItemTypeEnum.DistanceWeapon)]
+    public string bulletType;
+
+    [EnumPaging]
+    [ShowIf("itemType", ItemTypeEnum.DistanceWeapon)]
+    public ImpactEffectDistanceEnum impactEffectDistance;
+
+    [ShowIf("itemType", ItemTypeEnum.DistanceWeapon)]
+    public float weaponCooldownDistance;
+
+    [ShowIf("itemType", ItemTypeEnum.DistanceWeapon)]
+    public float cooldownDistance;
+
+
+    [EnumPaging]
+    [ShowIf("itemType", ItemTypeEnum.Consumables)]
+    public ConsumablesTypesEnum consumablesTypes;
+
+
+}
+
+public enum ItemTypeEnum
+{
+    CacWeapon, DistanceWeapon, Consumables
+}
+
+public enum ImpactEffectDistanceEnum
+{
+    Freeze, Fire, Poison, Stun, None
+}
+
+public enum impactEffectCacEnum
+{
+    Freeze, Fire, Poison, Stun, None
+}
+
+public enum ConsumablesTypesEnum
+{
+    Heal, Speed, Strenght
 }
