00e046b813686adf569dd770b09235782000d5d4
DESKTOP-3DBFBMB\Baptiste <baptiste.decarvalho@gmx.fr>
2021-01-20 19:12:26 +0100

    [Merge de Backup]

--- Assets/Script/Boss/Boss_WakeUp.cs
+++ Assets/Script/Boss/Boss_WakeUp.cs
@@ -4,13 +4,7 @@ using UnityEngine;
 
 public class Boss_WakeUp : MonoBehaviour
 {
-    
-    public firePoints points;
-    public enum firePoints
-    {
-        One_FP, Two_FP, Three_FP, Four_Fp, Five_Fp, Six_Fp, Seven_Fp, Eight_Fp, Nine_FP, Ten_Fp, 
-
-    }
+     
 
     public float bulletForce;
 
@@ -22,77 +16,176 @@ public class Boss_WakeUp : MonoBehaviour
 
     Vector2 fpDirection;
     public GameObject bulletPrefab;
-    public GameObject PremierFirepoint;
-    public GameObject DeuxiemeFirepoint;
-    public GameObject TroisiemeFirepoint;
-    public GameObject QuatrièmeFirepoint;
-    public GameObject CinquiemeFirepoint;
-    public GameObject SixiemeFirepoint;
-    public GameObject SeptiemeFirepoint;
-    public GameObject HuitiemeFirepoint;
-    public GameObject NeuviemeFirepoint;
-    public GameObject DixiemeFirepoint;
 
-    public Transform oneFP;
-    public Transform TwoFp;
-    public Transform ThreeFp;
-    public Transform FourFp;
-    public Transform FiveFp;
-    public Transform SixFp;
-    public Transform SevenFp;
-    public Transform EightFp;
-    public Transform NineFp;
-    public Transform TenFp;
-    
+    public Transform directionPoint;
+  
 
+    public bool isShooting;
 
-    private void Start()
+    public int numberOfAttackWaves;
+
+    private int coroutineCounter = 0;
+
+    void Start()
     {
-        Shoot = DoShoot;
-
+        StartCoroutine(nameof(DoShoot));
     }
-    private void DoShoot()
+    IEnumerator DoShoot()
     {
-        switch (points)
-        {
-            case firePoints.One_FP:
-                fpDirection = (oneFP.transform.position - transform.position).normalized * bulletForce;
+        coroutineCounter++;
+   
+            fpDirection = (directionPoint.transform.position - transform.position).normalized * bulletForce;
 
-                GameObject fireBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-                fireBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+        GameObject fireBulletOne = Instantiate(bulletPrefab, this.transform.position, this.transform.rotation) ;
+            fireBulletOne.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+       
+          
 
-                break;
+        /*        break;
+
             case firePoints.Two_FP:
+
+                fpDirection = (TwoFp.transform.position - transform.position).normalized * bulletForce;
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    GameObject fireBulletTwo = Instantiate(bulletPrefab, TwoFp.position, TwoFp.rotation);
+                    fireBulletTwo.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
+
                 break;
+
             case firePoints.Three_FP:
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    GameObject fireBulletThree = Instantiate(bulletPrefab, ThreeFp.position, ThreeFp.rotation);
+                    fireBulletThree.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
+
                 break;
             case firePoints.Four_Fp:
+
+                fpDirection = (FourFp.transform.position - transform.position).normalized * bulletForce;
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    GameObject fireBulletFour = Instantiate(bulletPrefab, FourFp.position, FourFp.rotation);
+                    fireBulletFour.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
+
                 break;
             case firePoints.Five_Fp:
+
+                fpDirection = (FiveFp.transform.position - transform.position).normalized * bulletForce;
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    GameObject fireBulletFive = Instantiate(bulletPrefab, FiveFp.position, FiveFp.rotation);
+                    fireBulletFive.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
+
                 break;
             case firePoints.Six_Fp:
+
+                fpDirection = (SixFp.transform.position - transform.position).normalized * bulletForce;
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    GameObject fireBulletSix = Instantiate(bulletPrefab, SixFp.position, SixFp.rotation);
+                    fireBulletSix.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
+
                 break;
             case firePoints.Seven_Fp:
+
+
+                fpDirection = (SevenFp.transform.position - transform.position).normalized * bulletForce;
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    GameObject fireBulletSeven = Instantiate(bulletPrefab, SevenFp.position, SevenFp.rotation);
+                    fireBulletSeven.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
                 break;
             case firePoints.Eight_Fp:
+
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    fpDirection = (EightFp.transform.position - transform.position).normalized * bulletForce;
+                    GameObject fireBulletEight = Instantiate(bulletPrefab, EightFp.position, EightFp.rotation);
+                    fireBulletEight.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
                 break;
+
             case firePoints.Nine_FP:
+
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    fpDirection = (NineFp.transform.position - transform.position).normalized * bulletForce;
+                    GameObject fireBulletEight = Instantiate(bulletPrefab, NineFp.position, NineFp.rotation);
+                    fireBulletEight.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+
+                }
                 break;
+
             case firePoints.Ten_Fp:
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    fpDirection = (NineFp.transform.position - transform.position).normalized * bulletForce;
+                    GameObject fireBulletEight = Instantiate(bulletPrefab, NineFp.position, NineFp.rotation);
+                    fireBulletEight.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+
+                }
+                break;
+
+            case firePoints.Eleven_FP:
+
+                for (int i = 0; i < numberOfAttackWaves; i++)
+                {
+                    fpDirection = (ElevenFp.transform.position - transform.position).normalized * bulletForce;
+                    GameObject fireBulletEight = Instantiate(bulletPrefab, ElevenFp.position, NineFp.rotation);
+                    fireBulletEight.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
+                    yield return new WaitForSeconds(shootIntervale);
+
+                }
                 break;
             default:
-                break;
+                break; */
+                        yield return new WaitForSeconds(shootIntervale);
+        if (coroutineCounter <= numberOfAttackWaves)
+        {
+            StartCoroutine(nameof(DoShoot));
+
+
         }
-
-     //   GameObject fireBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-          //  fireBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(fpDirection.x, fpDirection.y);
-        
+    }
 
 
-        Shoot = dontShoot;
 
     }
 
+    /*
     void dontShoot()
     {
         isShooting = false;
@@ -104,4 +197,5 @@ public class Boss_WakeUp : MonoBehaviour
         yield return new WaitForSeconds(shootIntervale);
         Shoot = DoShoot;
     }
-}
+    */
+
---
+++ Assets/Script/Procedural/EnsemblePS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnsemblePS : MonoBehaviour
+{
+
+    public GameObject[] PrefabLD;
+    private int rand;
+
+
+
+
+    void Start()
+    {
+        rand = Random.Range(0, PrefabLD.Length);
+        Instantiate(PrefabLD[rand], transform.position, PrefabLD[rand].transform.rotation);
+
+
+    }
+
+    
+}
