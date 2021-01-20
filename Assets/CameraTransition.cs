db3b95c17fba871478191815d89a85a1e665ced4
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2021-01-20 23:16:03 +0100

    Refonte menu principal

--- Assets/CameraTransition.cs
+++ Assets/CameraTransition.cs
@@ -6,11 +6,20 @@ public class CameraTransition : MonoBehaviour
 {
     public float xCoordinates;
     public float yCoordinates;
+    public Transform pC;
+    public float xPCCoordinates;
+    public float yPCCoordinates;
+
+    private void Start()
+    {
+        pC = GameObject.Find("Perso").GetComponent<Transform>();
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
             Camera.main.transform.position = transform.position + new Vector3(xCoordinates, yCoordinates, -10);
+            pC.position = transform.position + new Vector3(xPCCoordinates, yPCCoordinates, 0);
         }
     }
 }
