d756df3cb8d9b318e301395cb2b83fc0c336b121
DESKTOP-44DCLRC\Yovann <y.thieme@rubika-edu.com>
2021-01-20 08:30:58 +0100

    LE PUSH DE LA FATIGUE.

--- Assets/CameraTransition.cs
+++ Assets/CameraTransition.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CameraTransition : MonoBehaviour
 {
+    public float xCoordinates;
+    public float yCoordinates;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
         {
-            Camera.main.transform.position = transform.position + new Vector3(0, 10, -10);
+            Camera.main.transform.position = transform.position + new Vector3(xCoordinates, yCoordinates, -10);
         }
     }
 }
